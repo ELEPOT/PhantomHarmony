d0bcea9bf18d5711d0c0b394c3f3710fb4352cd6
ELEPOT <elepotmail0@gmail.com>
2023-11-06 21:25:45 +0800

    improving code flow

--- train.py
+++ train.py
@@ -8,19 +8,23 @@ DATA_DIR = 'D:/data'
 validation_images = [f'{DATA_DIR}/tests/{filename}' for filename in os.listdir(f'{DATA_DIR}/tests')]
 validation_prompts = [os.path.basename(img_path).split('.')[0] for img_path in validation_images]
 
-subprocess.call(
-    ['accelerate', 'launch',
-    f'diffusers/examples/controlnet/train_controlnet.py',
+command = []
 
+command += ['accelerate', 'launch', f'diffusers/examples/controlnet/train_controlnet.py']
+command += [
     f'--pretrained_model_name_or_path=runwayml/stable-diffusion-v1-5',
 
     f'--train_data_dir={DATA_DIR}/dataset/fill50k',
 
     f'--output_dir={DATA_DIR}/models',
     f'--cache_dir={DATA_DIR}/cache',
-    f'--logging_dir={DATA_DIR}/logs'] +
+    f'--logging_dir={DATA_DIR}/logs',
 
-    [f'--validation_image'] + validation_images + [f'--validation_prompt'] + validation_prompts +
+    f'--resolution=512',
+    f'--learning_rate=1e-5',
+    '--train_batch_size=4',
+]
 
-    [f'--resolution=512', f'--learning_rate=1e-5', '--train_batch_size=4']
-)
+command += [f'--validation_image'] + validation_images + [f'--validation_prompt'] + validation_prompts
+
+subprocess.call(command)
