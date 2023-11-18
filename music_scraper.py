3da13ad4ad68dbdfb513462ca833e1d270d3fc0f
ELEPOT <elepotmail0@gmail.com>
2023-11-18 08:35:21 +0800

    make music_scraper.py work

--- music_scraper.py
+++ music_scraper.py
@@ -3,51 +3,66 @@ import subprocess
 import os
 import itertools
 import shutil
-
-
-CLIENT_ID = '4dea824b6efe489b9c7c2c0c30822324'
-CLIENT_SECRET = 'b22341c0cca242c09e57b1695d512115'
-
-os.environ['SPOTIPY_CLIENT_ID'] = CLIENT_ID
-os.environ['SPOTIPY_CLIENT_SECRET'] = CLIENT_SECRET
-
-# modified from https://stackoverflow.com/questions/17547273/flatten-complex-directory-structure-in-python
-
+from config import *
+import ytmdl
 
 class Dataset:
-    def __init__(self, link_src, uri_column, output_dir=None):
+    def __init__(self, link_src, track_name_col, artists_col, output_dir=None, shuffle=False):
         if output_dir is None:
             self.output_dir = link_src.split('/')[1]
         else:
             self.output_dir = output_dir
 
-        self.dataset = load_dataset(link_src, split='train').shuffle(seed=42)
-        self.uri_column = uri_column
+        self.dataset = load_dataset(link_src, split='train')
+
+        if shuffle:
+            self.dataset = self.dataset.shuffle(seed=4242)
+
+        self.track_name_col = track_name_col
+        self.artist_col = artists_col
         self.index = 0
+        self.size = self.dataset.num_rows
 
-    def _move(self, destination):
-        output_file = ''
+    def pop(self, log=False):
+        track_name = self.dataset[self.track_name_col][self.index]
+        artists = self.dataset[self.artist_col][self.index]
 
-        for root, _dirs, files in itertools.islice(os.walk(destination), 1, None):
-            for filename in files:
-                shutil.move(os.path.join(root, filename), destination)
+        command = [
+            'ytmdl',
+            track_name,
+            '-q',
+            '--skip-meta',
+            '--ignore-errors',
+            '--on-meta-error' ,
+            'skip',
+            '--artist',
+            f'{artists}',
+            '-o',
+            f'{self.output_dir}',
+            '--filename',
+            f'{track_name}_{artists}'
+        ]
 
-                output_file = os.path.join(destination, filename)
-            os.rmdir(root)
 
-        return output_file
+        if log:
+            subprocess.call(command)
+        else:
+            subprocess.run(command)
 
-    def pop(self):
-        uri = self.dataset[self.uri_column][self.index]
-        subprocess.call(['spotify_dl', '-l', f'https://open.spotify.com/track/{uri}', '-o', self.output_dir])
-        output_file = self._move(self.output_dir)
+        print(command)
 
         self.index += 1
 
-        return output_file
+        return os.path.join(self.output_dir, f'{track_name}_{artists}.mp3')
 
 
 if __name__ == '__main__':
-    dataset = Dataset('maharshipandya/spotify-tracks-dataset', 'track_id')
-    print(dataset.pop())
-    print(dataset.pop())
+    dataset = Dataset(
+        link_src='maharshipandya/spotify-tracks-dataset',
+        track_name_col='track_name',
+        artists_col='artists',
+        output_dir=f'{DATA_DIR}/dataset/raw_music'
+    )
+
+    for i in range(dataset.size):
+        dataset.pop(log=True)
