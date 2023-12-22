4b564d6ef27be928f3eacdd26167b1151e1215e3
leo981008 <ke.leo981008@gmail.com>
2023-12-22 09:12:54 +0800

    Update webui.py

--- webui.py
+++ webui.py
@@ -1,20 +1,22 @@
-import shutil
-
 import gradio as gr
 
 
-def process_file(fileobj):
-    path = "F:\temp"
-    shutil.copyfile(fileobj.name, path)
-    return do_something_to_file(path)
+def zip_files(files):
+    '''
+    with ZipFile("tmp2222.zip", "w") as zipObj:
+        for idx, file in enumerate(files):
+            zipObj.write(file.name, file.name.split("/")[-1])
+    '''
 
+    with open('writeSomething.txt', 'w') as f:
+        f.write('hello\n')
+    return "writeSomething.txt"
 
 demo = gr.Interface(
-    fn=process_file,
-    inputs=[
-        "file",
-    ],
-    outputs="file",
+    zip_files,
+    gr.File(file_count="multiple", file_types=["text", ".json", ".csv"]),
+    "file",
 )
 
-demo.launch()
+if __name__ == "__main__":
+    demo.launch()
