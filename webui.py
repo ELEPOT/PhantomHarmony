9a3efac23f81d8bda2b19c25b2d15fa0be16d087
leo981008 <ke.leo981008@gmail.com>
2023-12-23 21:46:07 +0800

    finish 0.1

--- webui.py
+++ webui.py
@@ -10,13 +10,8 @@ def zip_files(text, files):
 
     with open("writeSomething.txt", "w") as f:
         a = files[0]
-<<<<<<< HEAD
         a = str(a + "   " + text)
         f.write(a + "\n")
-=======
-        a = str(a)
-        f.write(a)
->>>>>>> 516eec6fa789593cbb795b7d6698a6fc11b8676a
     return "writeSomething.txt"
 
 
@@ -26,11 +21,7 @@ demo = gr.Interface(
         gr.Textbox(lines=2, placeholder="Name Here..."),
         gr.File(file_count="multiple", file_types=["text", ".json", ".csv"]),
     ],
-<<<<<<< HEAD
     outputs="file",
-=======
-    outputs="text",
->>>>>>> 516eec6fa789593cbb795b7d6698a6fc11b8676a
 )
 
 if __name__ == "__main__":
