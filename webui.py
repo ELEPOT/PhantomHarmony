23209babc7d87024497187acb9e2e07d77533915
ELEPOT <elepotmail0@gmail.com>
2023-12-23 09:42:46 +0800

    reformatted webui.py

--- webui.py
+++ webui.py
@@ -1,21 +1,26 @@
 import gradio as gr
 
-def zip_files(text,files):
-    '''
+
+def zip_files(text, files):
+    """
     with ZipFile("tmp2222.zip", "w") as zipObj:
         for idx, file in enumerate(files):
             zipObj.write(file.name, file.name.split("/")[-1])
-    '''
+    """
 
-    with open('writeSomething.txt', 'w') as f:
-        a=files[0]
-        a=str(a)
+    with open("writeSomething.txt", "w") as f:
+        a = files[0]
+        a = str(a)
         f.write(a)
     return "writeSomething.txt"
 
+
 demo = gr.Interface(
     zip_files,
-    inputs=[gr.Textbox(lines=2, placeholder="Name Here..."),gr.File(file_count="multiple", file_types=["text", ".json", ".csv"])],
+    inputs=[
+        gr.Textbox(lines=2, placeholder="Name Here..."),
+        gr.File(file_count="multiple", file_types=["text", ".json", ".csv"]),
+    ],
     outputs="text",
 )
 
