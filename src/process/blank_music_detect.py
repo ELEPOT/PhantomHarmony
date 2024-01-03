a049c714c3d3a64f8aed61fc72beaaba2d4dd17d
ELEPOT <elepotmail0@gmail.com>
2024-01-03 16:25:52 +0800

    improve performance by using np.countzero

--- src/process/blank_music_detect.py
+++ src/process/blank_music_detect.py
@@ -1,18 +1,15 @@
 import os
 
 import librosa
+import numpy as np
 
 from paths import EXAMPLE_DIR
 
 y, sr = librosa.load(os.path.join(EXAMPLE_DIR, "fiction.mp3"), sr=None)
 
-blank = 0
 print(len(y))
-for i in range(len(y)):
-    # print(y[i])
-    if y[i] == 0:
-        # print(y[i],i)
-        blank = blank + 1
+blank = np.count_nonzero(y == 0)
+
 print(blank)
 avg = blank / len(y) * 100
 print(avg)
