6eb9547763ee493260b3c521ad877ee2b4163b4b
ELEPOT <elepotmail0@gmail.com>
2024-01-03 16:17:45 +0800

    Fix path problem

--- src/process/blank_music_detect.py
+++ src/process/blank_music_detect.py
@@ -1,12 +1,18 @@
+import os
+
 import librosa
-y, sr = librosa.load('/home/pi/PhantomHarmony/example/fiction.mp3',sr=None)
-blank=0
+
+from paths import EXAMPLE_DIR
+
+y, sr = librosa.load(os.path.join(EXAMPLE_DIR, "fiction.mp3"), sr=None)
+
+blank = 0
 print(len(y))
 for i in range(len(y)):
-    #print(y[i])
-    if y[i]==0:
-        #print(y[i],i)
-        blank=blank+1
+    # print(y[i])
+    if y[i] == 0:
+        # print(y[i],i)
+        blank = blank + 1
 print(blank)
-avg=blank/len(y)*100
+avg = blank / len(y) * 100
 print(avg)
