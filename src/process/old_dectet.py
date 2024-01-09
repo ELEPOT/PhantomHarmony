745f1341edf4d7309b8c888842bbcb3a79e781dc
leo981008 <ke.leo981008@gmail.com>
2024-01-09 12:50:16 +0000

    finish many files

--- src/process/old_dectet.py
+++ src/process/old_dectet.py
@@ -4,19 +4,12 @@ import librosa
 import numpy as np
 
 from paths import TEST_OUTPUT_DIR
-import pandas as pd
+
 input_dir = os.path.join(TEST_OUTPUT_DIR,"split_by_beat","fiction")
-df = pd.DataFrame()
-music_name = []
-files = [f for f in os.listdir(input_dir)]
-music_name=files
-print(files)
-folder_file_counts = {r:len([r+'/'+files for files in f]) for r,d,f in os.walk(input_dir)}
-files_count=folder_file_counts['/home/pi/PhantomHarmony/test_output/split_by_beat/fiction']
-blank_per=[]
+
 
 def detect_blank_music(path):
-    #print(path)
+    print(path)
     y, sr = librosa.load(path, sr=None)
 
     # print(len(y))
@@ -26,12 +19,10 @@ def detect_blank_music(path):
 
     return avg
 
-#folder_file_counts = {r:len([r+'/'+files for files in f]) for r,d,f in os.walk(input_dir)}
+folder_file_counts = {r:len([r+'/'+files for files in f]) for r,d,f in os.walk(input_dir)}
 #folder_file_counts=int(folder_file_counts)
-#print(folder_file_counts)
+print(folder_file_counts)
 for i in range(folder_file_counts['/home/pi/PhantomHarmony/test_output/split_by_beat/fiction']):
-    per=detect_blank_music(os.path.join(input_dir, files[i]))
-    blank_per.append(per)
-print(blank_per)
+    print(detect_blank_music(os.path.join(input_dir, "%05d" % (i+1)+".mp3")))
 #print(detect_blank_music(os.path.join(input_dir, "00001.mp3")))
 #print(detect_blank_music(os.path.join(input_dir, "00016.mp3")))
