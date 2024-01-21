22dc2ab7fbd097defe05a11fa3ac5b82472b8e61
ELEPOT <elepotmail0@gmail.com>
2024-01-21 14:20:00 +0800

    batch spectrogram

--- src/process/batch_spectrogram.py
+++ src/process/batch_spectrogram.py
@@ -14,16 +14,15 @@ converter = SpectrogramImageConverter(params)
 os.makedirs(os.path.join(DATA_DIR, "dataset", "spectrogram", "vocals"), exist_ok=True)
 os.makedirs(os.path.join(DATA_DIR, "dataset", "spectrogram", "accompaniment"), exist_ok=True)
 
-with open(os.path.join(DATA_DIR, "dataset", "blank_detect.csv")) as f:
+with open(os.path.join(DATA_DIR, "dataset", "split_by_time_sample.csv")) as f:
     reader = csv.DictReader(f)
 
     for row in reader:
-        if float(row["vocals_blank"]) < 0.3 and float(row["accompaniment_blank"]) < 0.7:
-            v_path = os.path.join(DATA_DIR, "dataset", "split_by_time", "vocals", row["music_name"] + ".mp3")
-            a_path = os.path.join(DATA_DIR, "dataset", "split_by_time", "accompaniment", row["music_name"] + ".mp3")
+        v_path = os.path.join(DATA_DIR, "dataset", "split_by_time", "vocals", row["music_name"] + ".mp3")
+        a_path = os.path.join(DATA_DIR, "dataset", "split_by_time", "accompaniment", row["music_name"] + ".mp3")
 
-            v_out_path = os.path.join(DATA_DIR, "dataset", "spectrogram", "vocals", row["music_name"] + ".png")
-            a_out_path = os.path.join(DATA_DIR, "dataset", "spectrogram", "accompaniment", row["music_name"] + ".png")
+        v_out_path = os.path.join(DATA_DIR, "dataset", "spectrogram", "vocals", row["music_name"] + ".png")
+        a_out_path = os.path.join(DATA_DIR, "dataset", "spectrogram", "accompaniment", row["music_name"] + ".png")
 
-            converter.spectrogram_image_from_audio(AudioSegment.from_mp3(v_path)).save(v_out_path)
-            converter.spectrogram_image_from_audio(AudioSegment.from_mp3(a_path)).save(a_out_path)
+        converter.spectrogram_image_from_audio(AudioSegment.from_mp3(v_path)).save(v_out_path)
+        converter.spectrogram_image_from_audio(AudioSegment.from_mp3(a_path)).save(a_out_path)
