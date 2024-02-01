b7e285919fe6bb8b842f1e6eb37559584f83342f
leo <ke.leo981008@gmail.com>
2024-02-01 20:56:34 +0800

    del

--- src/process/diff.py
+++ src/process/diff.py
@@ -1,26 +1,26 @@
-'''from diffusers import StableDiffusionPipeline
-#from diffusers import StableDiffusionControlNetPipeline, ControlNetModel
-import torch
-pipeline = StableDiffusionPipeline.from_single_file(
-    "/mnt/f/models/dreamshaper_8.safetensors"
-)
-pipeline.to("cuda")
-image = pipeline("An image of a squirrel in Picasso style").images[0]
-image.save("image_of_squirrel_painting.png")
-'''
-from diffusers import StableDiffusionControlNetPipeline, ControlNetModel
+from diffusers import StableDiffusionControlNetPipeline ,ControlNetModel
 import torch
 import cv2
-img = cv2.imread('blues.png')
+img = cv2.imread('/home/leo/blues.png')
+
+controlnet_model_path = "/mnt/d/models/diffusion_pytorch_model.safetensors"
+main_model_path = "/mnt/d/models/riffusion-model-v1.safetensors"
+
 controlnet = ControlNetModel.from_single_file(
-    "/mnt/d/models/diffusion_pytorch_model.safetensors", torch_dtype=torch.float16)
+    controlnet_model_path, torch_dtype=torch.float16
+)
 
 pipe = StableDiffusionControlNetPipeline.from_single_file(
-    "/mnt/d/models/riffusion-model-v1.ckpt", controlnet=controlnet, torch_dtype=torch.float16
+    main_model_path, controlnet=controlnet, torch_dtype=torch.float16
 )
-generator = torch.manual_seed(0)
+
+torch.manual_seed(0)
+generator = torch.random.manual_seed(0)
+
+output_path = "happy.png"
 
 out_image = pipe(
     "guitar", num_inference_steps=20, generator=generator, image=img
 ).images[0]
-image.save("happy.png")
+
+out_image.save(output_path)
