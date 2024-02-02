a9d9c5aee9392fa2efd12ed6d4d3b4478c91746e
leo <ke.leo981008@gmail.com>
2024-02-02 10:28:26 +0800

    unfinish

--- src/process/diff.py
+++ src/process/diff.py
@@ -6,13 +6,9 @@ img = cv2.imread('/home/leo/blues.png')
 controlnet_model_path = "/mnt/d/models/diffusion_pytorch_model.safetensors"
 main_model_path = "/mnt/d/models/riffusion-model-v1.safetensors"
 
-controlnet = ControlNetModel.from_single_file(
-    controlnet_model_path, torch_dtype=torch.float16
-)
+controlnet = ControlNetModel.from_single_file(controlnet_model_path, torch_dtype=torch.float16)
 
-pipe = StableDiffusionControlNetPipeline.from_single_file(
-    main_model_path, controlnet=controlnet, torch_dtype=torch.float16
-)
+pipe = StableDiffusionControlNetPipeline.from_pretrained("riffusion/riffusion-model-v1", controlnet=controlnet, torch_dtype=torch.float16)
 
 torch.manual_seed(0)
 generator = torch.random.manual_seed(0)
