ad32982302bbcee5b27737164b0252ee788d54e6
ELEPOT <elepotmail0@gmail.com>
2024-02-05 18:25:32 +0800

    listdir -> spotify_144k.csv

--- src/process/split_by_beat.py
+++ src/process/split_by_beat.py
@@ -5,12 +5,11 @@ from pathlib import Path
 from paths import DATASET_DIR
 import torchaudio
 import librosa
+import pandas as pd
 
 # parameters
 input_dir = DATASET_DIR / "spleeter"
 out_dir = DATASET_DIR / "split_by_beat"
-
-dirs = [f for f in os.listdir(input_dir)]
 
 beat_per_section = 8
 
@@ -19,10 +18,8 @@ start_time = time.time()
 
 def beat(beat_path, vocals_path, accompaniment_path, out_path):
     track_id = Path(beat_path).stem
-    print(track_id)
 
     if os.path.exists(out_path / "vocals" / f"{track_id}_00001.mp3"):
-        print("skip")
         return
 
     y, sr = torchaudio.load(beat_path)
@@ -63,7 +60,12 @@ def beat(beat_path, vocals_path, accompaniment_path, out_path):
             )
 
 
-for f in dirs:
+for i, f in pd.read_csv(DATASET_DIR / "spotify_114k.csv").iterrows():
+    f = f["track_id"]
+
+    if i % 100 == 0:
+        print(i, f)
+
     beat(
         DATASET_DIR / "spotify_114k" / (f + ".mp3"),
         input_dir / f / "vocals.mp3",
