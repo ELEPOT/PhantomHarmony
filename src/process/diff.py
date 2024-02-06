13bbcb0dc638f889684a7817f8907faa8f5ee99f
ELEPOT <elepotmail0@gmail.com>
2024-02-06 12:17:13 +0800

    yeaaahhhhhhhhhhh

--- src/process/diff.py
+++ src/process/diff.py
@@ -1,26 +1,42 @@
-from diffusers import StableDiffusionControlNetPipeline ,ControlNetModel
+from diffusers import *
+from transformers import *
+from diffusers.schedulers import PNDMScheduler
+from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 import torch
-import cv2
-img = cv2.imread('/home/leo/blues.png')
+from PIL import Image
 
-controlnet_model_path = "/mnt/d/models/diffusion_pytorch_model.safetensors"
-#main_model_path = "/mnt/d/models/riffusion-model-v1.safetensors"
-#url = "https://huggingface.co/leo1008/test/main/diffusion_pytorch_model.safetensors"  # can also be a local path
-controlnet = ControlNetModel.from_pretrained("leo1008/test", torch_dtype=torch.float16)
+torch.set_default_device("cuda")
 
-pipe = StableDiffusionControlNetPipeline.from_pretrained("riffusion/riffusion-model-v1", controlnet=controlnet, torch_dtype=torch.float16)
+img = Image.open("/mnt/g/nextcloud_data/tests/blues.png")
+
+controlnet_model_path = "/mnt/g/nextcloud_data/models/first-7500/controlnet"
+main_model_path = "/mnt/g/nextcloud_data/models/riffusion/unet"
+
+# url = "https://huggingface.co/leo1008/test/main/diffusion_pytorch_model.safetensors"  # can also be a local path
+# controlnet = ControlNetModel.from_pretrained("leo1008/test", torch_dtype=torch.float16)
+
+controlnet = ControlNetModel.from_pretrained(controlnet_model_path)
+main_model = UNet2DConditionModel.from_pretrained(main_model_path)
+vae = AutoencoderKL.from_pretrained("/mnt/g/nextcloud_data/models/riffusion/vae")
+text_encoder = CLIPTextModel.from_pretrained("/mnt/g/nextcloud_data/models/riffusion/text_encoder")
+tokenizer = CLIPTokenizer.from_pretrained("/mnt/g/nextcloud_data/models/riffusion/tokenizer")
+scheduler = PNDMScheduler.from_pretrained("/mnt/g/nextcloud_data/models/riffusion/scheduler")
+safety_checker = StableDiffusionSafetyChecker.from_pretrained("/mnt/g/nextcloud_data/models/riffusion/safety_checker")
+feature_extractor = CLIPImageProcessor.from_pretrained("/mnt/g/nextcloud_data/models/riffusion/feature_extractor")
+
+pipe = StableDiffusionControlNetPipeline(
+    vae, text_encoder, tokenizer, main_model, controlnet, scheduler, safety_checker, feature_extractor
+).to("cuda")
 
 torch.manual_seed(0)
 generator = torch.random.manual_seed(0)
 
 output_path = "happy.png"
 
-out_image = pipe(
-    "guitar", num_inference_steps=20, generator=generator, image=img
-).images[0]
+out_image = pipe("guitar", num_inference_steps=20, generator=generator, image=img).images[0]
 
 out_image.save(output_path)
-'''
+"""
 error msg
 
 Traceback (most recent call last):
@@ -33,4 +49,4 @@ Traceback (most recent call last):
   File "/home/leo/test/lib/python3.10/site-packages/diffusers/models/modeling_utils.py", line 154, in load_model_dict_into_meta
     raise ValueError(
 ValueError: Cannot load leo1008/test because down_blocks.0.attentions.0.transformer_blocks.0.attn2.to_k.weight expected shape tensor(..., device='meta', size=(320, 1280)), but got torch.Size([320, 768]). If you want to instead overwrite randomly initialized weights, please make sure to pass both `low_cpu_mem_usage=False` and `ignore_mismatched_sizes=True`. For more information, see also: https://github.com/huggingface/diffusers/issues/1619#issuecomment-1345604389 as an example.
-'''
+"""
