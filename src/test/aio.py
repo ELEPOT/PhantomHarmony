0426ad617e6b5313178ec14abd79bfd4b79bc766
ELEPOT <elepotmail0@gmail.com>
2024-02-06 11:09:49 +0800

    add helper text

--- src/test/aio.py
+++ src/test/aio.py
@@ -5,19 +5,36 @@ from pydub import AudioSegment
 from PIL import Image
 import sys
 import subprocess
+from colorama import *
 
-cmd = "spleeter separate "+
+# cmd = "spleeter separate "+
 
-if str(sys.argv[1]) == "m2s":
-    params = SpectrogramParams()
-    converter = SpectrogramImageConverter(params)
+try:
+    if str(sys.argv[1]) == "m2s":
+        params = SpectrogramParams()
+        converter = SpectrogramImageConverter(params)
 
-    seg = AudioSegment.from_mp3(str(sys.argv[2]))
-    seg = seg.set_frame_rate(44100)
-    converter.spectrogram_image_from_audio(seg).save(str(sys.argv[3]))
-elif str(sys.argv[1]) == "s2m":
-    params = SpectrogramParams()
-    converter = SpectrogramImageConverter(params)
+        seg = AudioSegment.from_file(str(sys.argv[2]))
+        seg = seg.set_frame_rate(44100)
+        converter.spectrogram_image_from_audio(seg).save(str(sys.argv[3]))
 
-    img = Image.open(str(sys.argv[2]))
-    converter.audio_from_spectrogram_image(img).export(str(sys.argv[3]))
+    elif str(sys.argv[1]) == "s2m":
+        params = SpectrogramParams()
+        converter = SpectrogramImageConverter(params)
+
+        img = Image.open(str(sys.argv[2]))
+        converter.audio_from_spectrogram_image(img).export(str(sys.argv[3]))
+
+except:
+    help_text = """
+    Usage:
+        python aio.py (m2s / s2m) (in_path) (out_path)
+    
+    Commands:
+        m2s: 
+            Convert music to spectrogram
+        s2m: 
+            Convert spectrogram to music
+    """
+
+    print(Fore.RED + help_text)
