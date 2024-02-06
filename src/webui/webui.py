943ca2d2612956a1200d7e1e22d16d0d751bad84
leo <ke.leo981008@gmail.com>
2024-02-06 21:02:43 +0800

    webui conncet models 2

--- src/webui/webui.py
+++ src/webui/webui.py
@@ -1,24 +1,22 @@
 import gradio as gr
-
-
+import aio.py
+import diff.py
 def zip_files(text, files):
+    in_put = os.path.split(files[0])
     """
     with ZipFile("tmp2222.zip", "w") as zipObj:
         for idx, file in enumerate(files):
             zipObj.write(file.name, file.name.split("/")[-1])
     """
     if files == None or text == "":
-        with open("writeSomething.txt", "w") as f:
-            
-            
+        with open("error.txt", "w") as f:
             f.write( "please write command here.And upload music file to next block. \n")
         return "writeSomething.txt","please write command here.And upload music file to next block. \n"+text
     else:
-        with open("writeSomething.txt", "w") as f:
-            a = files[0]
-            a = str(a + "   " + text)
-            f.write(a + "\n")
-        return "writeSomething.txt","SUCCESSFUL!!!"+text
+        run("m2s",files[0],in_put[0]+"unf1.png")
+        diff(in_put[0]+"unf2.png",in_put[0]+"unf1.png")
+        run("s2m",in_put[0]+"unf1.png",in_put[0]+"finish.mp3")
+        return "finish.mp3","SUCCESSFUL!!!"+text
 
 
 demo = gr.Interface(
