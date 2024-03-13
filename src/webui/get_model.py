b09d6caed7fd7ec57eff3a3505cbe5ad2993e4d1
leo <ke.leo981008@gmail.com>
2024-03-13 12:53:19 +0800

    fix get model

--- src/webui/get_model.py
+++ src/webui/get_model.py
@@ -1,8 +1,10 @@
 import wget
-
-hash_url = "hash_url"
-with open("hash.txt", "r") as f:
-    hash_old = f.read()
+hash_url = "download.elepot.dev/hash.txt"
+try:
+    with open("hash.txt", "r") as f:
+        hash_old = f.read()
+except:
+    hash_old = 0
 new = wget.download(hash_url)
 with open(new, "r") as f:
     hash_new = f.read()
