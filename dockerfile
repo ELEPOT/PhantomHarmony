cfe430ccaf5cbbdd19dfd5f97976b3d8e243e4de
leo <ke.leo981008@gmail.com>
2024-03-25 12:45:25 +0800

    update docker file

--- dockerfile
+++ dockerfile
@@ -10,4 +10,4 @@ RUN apt-get update -y
 RUN apt-get install ffmpeg -y
 RUN python3.9 -m pip install --no-deps -r /gradio/webui_require.txt
 #RUN apt-get install ffmpeg
-#CMD ["pip","install","-r", "/gradio/webui_require.txt"]
+CMD ["python", "/gradio/src/webui/webui.py"]
--- src/webui/get_model.py
+++ src/webui/get_model.py
@@ -1,5 +1,5 @@
 import wget
-hash_url = "download.elepot.dev/hash.txt"
+hash_url = "https://link.storjshare.io/s/jw43zc2rlwjv26ebnrkzyzzl24ya/model%2Fhash.txt?download=1"
 try:
     with open("hash.txt", "r") as f:
         hash_old = f.read()
@@ -9,5 +9,5 @@ new = wget.download(hash_url)
 with open(new, "r") as f:
     hash_new = f.read()
 if hash_old != hash_new:
-    url = "url"
+    url = "https://link.storjshare.io/s/jv57f7htl5khe5krqbcp7qnjk7pa/model%2Fdreamshaper_8.safetensors?download=1"
     filename = wget.download(url)
