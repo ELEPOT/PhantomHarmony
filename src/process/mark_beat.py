a9cf1d3be84f710dec9c5c9e39b7cbd5be95b9d4
ELEPOT <elepotmail0@gmail.com>
2024-04-04 10:16:32 +0800

    add control to beat mark thickness

--- src/process/mark_beat.py
+++ src/process/mark_beat.py
@@ -6,7 +6,7 @@ import numpy as np
 import os
 
 
-def mark_beat(output_spectrogram_path, input_audio_path, input_spectrogram_path):
+def mark_beat(output_spectrogram_path, input_audio_path, input_spectrogram_path, mark_radius=0):
     if os.path.isfile(output_spectrogram_path) and output_spectrogram_path != input_spectrogram_path:
         return
 
@@ -21,7 +21,7 @@ def mark_beat(output_spectrogram_path, input_audio_path, input_spectrogram_path)
 
         for beat_col in beat_cols:
             beat_col = int(beat_col)
-            img[:, beat_col, 0] = 255
+            img[:, beat_col - mark_radius : beat_col + mark_radius + 1, 0] = 255
 
     except ValueError:
         print(f"failed to find beat of {input_audio_path}")
@@ -32,10 +32,11 @@ def mark_beat(output_spectrogram_path, input_audio_path, input_spectrogram_path)
 if __name__ == "__main__":
     split_by_time_sample = pd.read_csv(DATASET_DIR / "split_by_time_sample.csv")
 
-    os.makedirs(DATASET_DIR / "spectrogram" / "vocals_beat_mark", exist_ok=True)
+    os.makedirs(DATASET_DIR / "spectrogram" / "vocals_beat_mark_width_5", exist_ok=True)
     for i, row in split_by_time_sample.iterrows():
         mark_beat(
-            DATASET_DIR / "spectrogram" / "vocals_beat_mark" / f"{row['music_name']}.png",
+            DATASET_DIR / "spectrogram" / "vocals_beat_mark_width_5" / f"{row['music_name']}.png",
             DATASET_DIR / "split_by_time" / "accompaniment" / f"{row['music_name']}.mp3",
             DATASET_DIR / "spectrogram" / "vocals" / f"{row['music_name']}.png",
+            mark_radius=2,
         )
